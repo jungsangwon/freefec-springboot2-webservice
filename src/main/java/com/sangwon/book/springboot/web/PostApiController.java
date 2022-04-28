e7c91590a9abc48eed1a4903f78c9d959ed6891d
sangwonjung <jungsangwon>
2022-04-28 18:04:23 +0900

    API insert/update/select

---
+++ src/main/java/com/sangwon/book/springboot/web/PostApiController.java
@@ -0,0 +1,30 @@
+package com.sangwon.book.springboot.web;
+
+import com.sangwon.book.springboot.service.posts.PostsService;
+import com.sangwon.book.springboot.web.dto.PostsResponseDto;
+import com.sangwon.book.springboot.web.dto.PostsSaveRequestDto;
+import com.sangwon.book.springboot.web.dto.PostsUpdateRequestDto;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
